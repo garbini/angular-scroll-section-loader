1689d386308a7ead2497780779afb71096e1589a
Fábio Garbini <fabio@garbini.net>
2016-03-09 13:30:29 -0300

    Remove duplicated var declaration

--- scroll-section-loader.js
+++ scroll-section-loader.js
@@ -16,8 +16,6 @@ angular.module('scrollSectionLoader', []).
         $window.bind('scroll', function(event) {
           var scrollPos = (document.body.scrollTop || document.documentElement.scrollTop) + document.documentElement.clientHeight;
           var elemBottom = element[0].offsetTop + element[0].offsetHeight;
-          var scrollPos = (document.body.scrollTop || document.documentElement.scrollTop) + document.documentElement.clientHeight;
-          var elemBottom = element[0].offsetTop + element[0].offsetHeight;
           if(scrollPos >= elemBottom) { //scrolled to bottom of scrollSection element
             $window.unbind(event); //this listener is no longer needed.
             if(loadedSections.length < sections.length) { //if there are still elements to load
